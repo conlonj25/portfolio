71e099fab2713fd9f8166c6c897589f93bf2a259
James Conlon <james.conlon@fr.fcm.travel>
2023-10-21 13:45:58 +0200

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,25 @@ import Image from 'next/image';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 
+import { Button } from '@/components/ui/button';
+import { ModeToggle } from './components/ModeToggle';
+
 export default function Home() {
 	return (
 		<>
-			<div className="p-5 bg-slate-500 mb-5">
+			<div className="p-5 bg-slate-500 mb-5 flex justify-between">
 				<div className="text-3xl font-bold text-white">
 					James Conlon
 				</div>
+				<ModeToggle />
 			</div>
+
+			<div>
+				<Button className="bg-warning text-warning-foreground">
+					Click me
+				</Button>
+			</div>
+
 			<div className="container mx-auto">
 				<div className="mb-5">
 					<Skills />
